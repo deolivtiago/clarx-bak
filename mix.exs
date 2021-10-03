a85c3303c397a8779250f803a0ca0fede6d55bb5
Tiago Oliveira <olvrti@gmail.com>
2021-10-02 23:27:03 -0300

    chore: include credo 1.5

--- mix.exs
+++ mix.exs
@@ -33,6 +33,7 @@ defmodule Clarx.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:phoenix, "~> 1.6.0"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
